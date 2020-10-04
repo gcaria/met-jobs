bb1dce7347b2d79691298125b9a4f1502a6ae7f6
Giacomo Caria <giacomo.caria@gmail.com>
2020-10-04 17:28:46 +0200

    Simplify code

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
 """Tools to build database for search engine."""
-import sys
 from urllib.parse import urljoin
 from requests import get
 import requests
 from bs4 import BeautifulSoup as bs
 import ssl
-import csv
+import pandas as pd
 
 ssl._create_default_https_context = ssl._create_unverified_context
 requests.packages.urllib3.disable_warnings()
@@ -16,19 +15,13 @@ soup = bs(get(URL_ROOT, verify=False).text, "lxml")
 
 
 class Database:
-    def __init__(self, years, months, path_csv, n_lines_max=int(1E6)):
+    def __init__(self, n_lines_max=int(1E6)):
 
-        self.years = years
-        self.months = months
-        self.path_csv = path_csv
+        self.titles = []
+        self.dates = []
+        self.urls = []
         self.n_lines_max = n_lines_max
-        self._init_csv()
         self._loop()
-
-    def _init_csv(self):
-
-        with open(self.path_csv, "w") as f:
-            pass
 
     def _loop(self):
 
@@ -44,26 +37,49 @@ class Database:
                 # for each link in a month page
                 for ad_link in monthSoup.findAll('a'):
 
-                    ad_href = ad_link.get('href')
+                    title, date, ad_url = extract_ad(ad_link, month_url)
+                    if title:
+                        self._append(title, date, ad_url)
 
-                    if ad_href and 'msg' in ad_href:
+                    if len(self.titles) == self.n_lines_max:
+                        break
 
-                        ad_url = month_url.replace('date.html', ad_href)
+    def _append(self, title, date, url):
 
-                        ad_soup = bs(get(
-                            ad_url, verify=False).text, "lxml")
+        print(title, date, url)
+        self.titles.append(title)
+        self.dates.append(date)
+        self.urls.append(title)
+        print(len(self.titles))
 
-                        title = ad_soup.find('h1').get_text()
+    @property
+    def df(self):
+        return pd.DataFrame(list(zip(self.titles, self.dates, self.urls)),
+                            columns=['title', 'date', 'url'])
 
-                        date = [a.get_text() for a in ad_soup.select(
-                            'tr td') if a.get_text().count(':') == 2][0]
+    def to_csv(self, path):
+        self.df.to_csv(self.path)
 
-                        print(title, date, ad_url)
-                        with open(self.path_csv, 'a') as newFile:
-                            new = csv.writer(newFile)
-                            new.writerow([title, date, ad_url])
 
-                        with open(self.path_csv) as f:
-                            n_lines = sum(1 for line in f)
-                            if n_lines == self.n_lines_max:
-                                sys.exit()
+def extract_ad(ad_link, month_url):
+
+    title, date, ad_url = None, None, None
+
+    ad_href = ad_link.get('href')
+
+    if ad_href and 'msg' in ad_href:
+
+        ad_url = month_url.replace('date.html', ad_href)
+
+        ad_soup = bs(get(
+            ad_url, verify=False).text, "lxml")
+
+        title = ad_soup.find('h1').get_text()
+
+        dates = [a.get_text() for a in ad_soup.select(
+            'tr td') if a.get_text().count(':') == 2]
+
+        if dates:
+            date = dates[0]
+
+    return title, date, ad_url
