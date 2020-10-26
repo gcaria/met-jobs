26ff39feb7e91f91055e80228c534849196aa3a6
Giacomo Caria <giacomo.caria@gmail.com>
2020-10-26 12:32:05 +0100

    Add arguments, improve display

--- search.py
+++ search.py
@@ -2,18 +2,29 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import argparse
+from style import printc
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-q', '--query', required=True)
+parser.add_argument('-q', '--query', required=True,
+                    help='String for search query')
+parser.add_argument('-d', '--database', default='data/database.csv',
+                    help='Database for search query')
+parser.add_argument('-n', '--n_results', default=10, type=int,
+                    help='Number of results displayed')
 args = parser.parse_args()
 
-df = pd.read_csv('database.csv')
+df = pd.read_csv(args.database)
+
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(df['title'])
 query_vec = vectorizer.transform([args.query])
-results = cosine_similarity(X,query_vec).reshape((-1,))
+results = cosine_similarity(X, query_vec).reshape((-1,))
 
+for i_count, i_df in enumerate(results.argsort()[-args.n_results:][::-1]):
 
-for i_count, i_df in enumerate(results.argsort()[-10:][::-1]):
-    print(i_count+1, df.loc[i_df,'title'])
-    print(df.loc[i_df,'url'])
+    print('-'*70)
+    i_title = df.loc[i_df, 'title']
+    i_date = df.loc[i_df, 'date']
+    i_url = df.loc[i_df, 'url']
+    printc(f'{i_count+1}) {i_title} - {i_date}', 'blue')
+    print(f'{i_url}')
