93e3c2040cc8293749fe26b9cc2b0327a50ccdad
Giacomo Caria <giacomo.caria@gmail.com>
2021-05-25 22:12:00 +0200

    Change file structure to make package pip-installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="met-jobs",
+    version="1.0",
+    author="Giacomo Caria",
+    author_email="giacomo.caria@gmail.com",
+    packages=["met_jobs"],
+)
